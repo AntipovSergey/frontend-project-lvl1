8d03119442819600c124e8a4dbff0185a7bb77c6
Antipov Sergey <antip99999@mail.ru>
2022-01-17 15:02:04 +0300

    fix console.log condition in isNumberEven.js

--- src/isNumberEven.js
+++ src/isNumberEven.js
@@ -4,10 +4,10 @@ import getRandomNubmer from './randomNumber.js';
 
 const isNumberEven = () => {
     let result = 0;
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
     while (result !== 3) {
         const randomNumber = getRandomNubmer();
-        const gameQuestion = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no".
-Question: ${randomNumber} `);
+        const gameQuestion = readlineSync.question(`Question: ${randomNumber} `);
         console.log(`Your answer: ${gameQuestion}`);
         if ((randomNumber % 2 === 0 && gameQuestion === 'yes') || (randomNumber % 2 !== 0 && gameQuestion === 'no')) {
             console.log('Correct!');
@@ -15,7 +15,6 @@ Question: ${randomNumber} `);
         } else if (randomNumber % 2 !== 0 && gameQuestion === 'yes') {
             return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.
 Let's try again, ${name}!`);
-
         } else if (randomNumber % 2 === 0 && gameQuestion === 'no') {
             return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.
 Let's try again, ${name}!`);
@@ -23,7 +22,7 @@ Let's try again, ${name}!`);
             return console.log('Correct input should be "yes" or "no"');
         }
     }
-    console.log(`Congratulations, ${name}!`);
+    return console.log(`Congratulations, ${name}!`);
 };
 
 export default isNumberEven;
