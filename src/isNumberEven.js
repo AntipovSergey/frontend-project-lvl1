47f864b8fb082a05cfef390a8c2d5ea4090b1ba8
Antipov Sergey <antip99999@mail.ru>
2022-01-17 17:44:05 +0300

    fix answer in isNumberEven.js

--- src/isNumberEven.js
+++ src/isNumberEven.js
@@ -7,18 +7,18 @@ const isNumberEven = () => {
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
     while (result !== 3) {
         const randomNumber = getRandomNubmer();
-        const gameQuestion = readlineSync.question(`Question: ${randomNumber} `);
-        console.log(`Your answer: ${gameQuestion}`);
-        if ((randomNumber % 2 === 0 && gameQuestion === 'yes') || (randomNumber % 2 !== 0 && gameQuestion === 'no')) {
+        console.log(`Question: ${randomNumber}`);
+        const gameAnswer = readlineSync.question('Your answer: ');
+        if ((randomNumber % 2 === 0 && gameAnswer === 'yes') || (randomNumber % 2 !== 0 && gameAnswer === 'no')) {
             console.log('Correct!');
             result++;
-        } else if (randomNumber % 2 !== 0 && gameQuestion === 'yes') {
+        } else if (randomNumber % 2 !== 0 && gameAnswer === 'yes') {
             return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.
 Let's try again, ${name}!`);
-        } else if (randomNumber % 2 === 0 && gameQuestion === 'no') {
+        } else if (randomNumber % 2 === 0 && gameAnswer === 'no') {
             return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.
 Let's try again, ${name}!`);
-        } else if (gameQuestion !== 'yes' || gameQuestion !== 'no') {
+        } else if (gameAnswer !== 'yes' || gameAnswer !== 'no') {
             return console.log('Correct input should be "yes" or "no"');
         }
     }
