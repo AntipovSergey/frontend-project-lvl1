0e813f554b714526da7eb42af48fae98442ebcdc
Antipov Sergey <antip99999@mail.ru>
2022-01-17 17:40:26 +0300

    create brain-clac.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { getUserName } from '../src/cli.js';
+import calculateRandomExpression from '../src/calculateRandomExpression.js';
+
+getUserName();
+calculateRandomExpression();
