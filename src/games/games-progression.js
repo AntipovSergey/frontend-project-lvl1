41f304689142219997232b7b635accd0710b0df0
Antipov Sergey <antip99999@mail.ru>
2022-01-21 14:36:31 +0300

    edit games-progression.js

--- src/games/games-progression.js
+++ src/games/games-progression.js
@@ -2,28 +2,26 @@ import generateGame from '../index.js';
 import getRandomNumber from '../randomNumber.js';
 
 const gameQuestion = 'What number is missing in the progression?';
+const step = getRandomNumber(2, 5);
+const firstNumber = getRandomNumber(1, 10);
 
-const generateArithmeticProgression = () => {
+const generateArithmeticProgression = (init, move) => {
   const result = [];
-  const step = getRandomNumber(2, 5);
-  const firstNumber = getRandomNumber(1, 10);
-  let temp = 0;
 
-  for (let i = 0; i <= 10; i += 1) {
-    temp += firstNumber + step;
-    result.push(temp);
+  for (let i = 0; i < 10 * move; i += move) {
+    result.push(init + i);
   }
 
   return result;
 };
 
-const createArrayWithCoveredElem = (arr) => {
-  const randomElement = arr.splice(getRandomNumber(0, arr.length), 1, '..');
+const createGameArray = (arr) => {
+  const randomElement = arr.splice(getRandomNumber(0, arr.length - 1), 1, '..');
   return [arr, randomElement];
 };
 
 const gameQuestionAnswer = () => {
-  const [arr, randomElement] = createArrayWithCoveredElem(generateArithmeticProgression());
+  const [arr, randomElement] = createGameArray(generateArithmeticProgression(firstNumber, step));
   const question = arr.join(' ');
   const answer = String(randomElement);
   return [question, answer];
