60e4b0e5a62918a5acfd96b8574553d2818ffb1d
Antipov Sergey <antip99999@mail.ru>
2022-01-25 12:39:37 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import readlineSync from 'readline-sync';
 
 const generateGame = (gameQuestion, gameQuestionAnswer) => {
-  let result = 0;
-  const attempts = 3;
   const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(gameQuestion);
 
-  while (result !== attempts) {
+  for (let i = 0; i < 3; i += 1) {
     const [question, answer] = gameQuestionAnswer();
     console.log(`Question: ${question}`);
     const yourAnswer = readlineSync.question('Your answer: ');
+
     if (yourAnswer === answer) {
       console.log('Correct');
-      result += 1;
     } else {
       return console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\n Let's try again, ${name}!`);
     }
